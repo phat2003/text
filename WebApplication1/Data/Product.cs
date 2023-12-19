e46183883c30985745c599b57a5b15997cd1a1bd
phat2003 <phamanhtienphat@gmail.com>
2023-12-19 09:36:06 +0700

    Add project files.

---
+++ WebApplication1/Data/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Data
+{
+    [Table("Product")]
+
+    public partial class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Author { get; set; }
+        public string Information { get; set; }
+        public string Image { get; set; }
+
+    }
+}
